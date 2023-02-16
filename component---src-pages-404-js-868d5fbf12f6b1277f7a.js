cdc2a894c117c2e65f0b61621d280103661756df
Heiss <Heiss@users.noreply.github.com>
2023-02-16 19:07:57 +0000

    deploy: ff6d3fcc7961798c6ec84ed6d7c40da1b021fcd0

---
+++ component---src-pages-404-js-868d5fbf12f6b1277f7a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkskohub_vocabs=self.webpackChunkskohub_vocabs||[]).push([[883],{429:function(t,s,e){e.r(s);e(7294);var u=e(4110),a=e(8183),l=e(9124);s.default=()=>(0,l.tZ)(u.Z,null,(0,l.tZ)("div",{className:"centerPage"},(0,l.tZ)(a.Z,{title:"404: Not found"}),(0,l.tZ)("h1",null,"NOT FOUND"),(0,l.tZ)("p",null,"You just hit a route that doesn't exist... the sadness.")))}}]);
+//# sourceMappingURL=component---src-pages-404-js-868d5fbf12f6b1277f7a.js.map
